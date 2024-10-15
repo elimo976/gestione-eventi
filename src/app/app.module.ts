a8705fe6c7c175b3e3e7593f0411ac3679307e3c
username / Elisa + Morreale <elisamorreale@gmail.com>
2024-10-15 13:41:16 +0200

    creazione footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { CookieConsentComponent } from './components/cookie-consent/cookie-consent.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { EventsPreviewComponent } from './components/events-preview/events-preview.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebookF, faXTwitter, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
   declarations: [
@@ -18,13 +21,19 @@ import { EventsPreviewComponent } from './components/events-preview/events-previ
     HeaderComponent,
     CookieConsentComponent,
     HeroComponent,
-    EventsPreviewComponent
+    EventsPreviewComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faFacebookF, faXTwitter, faInstagram, faLinkedinIn);
+  }
+}
